da179f3ee2e8b2830e4692934f574f03bbe1e5ec
Max Petschack <18609293+Midataur@users.noreply.github.com>
2026-01-20 15:15:19 +1100

    smallandshort-6
    more data

--- experiments/core/main.py
+++ experiments/core/main.py
@@ -1,5 +1,5 @@
 CONFIG = {
-    "modelname": "smallandshort-5",
+    "modelname": "smallandshort-6",
     "model_type": "RegressionModel",
     "dataset": "smallandshort",
     "dataset_type": "basicregression",
