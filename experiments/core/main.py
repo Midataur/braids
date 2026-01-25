39a7bc5ae84ee63a479800ec2fa55a2490d11283
Max Petschack <18609293+Midataur@users.noreply.github.com>
2026-01-26 10:09:24 +1100

    smallandshort-23

--- experiments/core/main.py
+++ experiments/core/main.py
@@ -1,5 +1,5 @@
 CONFIG = {
-    "modelname": "smallandshort-22",
+    "modelname": "smallandshort-23",
     "model_type": "RegressionModel",
     "dataset": "smallandshort",
     "dataset_type": "basicregression",
@@ -33,7 +33,7 @@ CONFIG = {
     "batchsize": 512, 
 
     # good starting value: 0.1
-    "weight_decay": 0.01, 
+    "weight_decay": 0.0001, 
 
     # usually 0.1
     "lr_factor": 0.1, 
